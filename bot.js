ea8f9e90e2bf3f6da921cf89470c1e18de4664ca
Alex Tedesco <alextede8899@gmail.com>
2020-12-12 08:31:27 -0500

    Initial commit

---
+++ bot.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const discord = require('discord.js');
+const client = new discord.Client();
+
+client.login(process.env.BOT_TOKEN);
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in.`);
+})
+
+client.on('message', msg => {
+    if(msg.author.bot) return;
+    if(msg.content.toLowerCase() === "dirty buck" || msg.content.toLowerCase() === "dirtybuck") {
+        msg.channel.send({
+            files: ['https://cdn.discordapp.com/attachments/477936186678050835/785732233818144808/Dirty_Buck_slays_v4.png']
+        }).then(console.log).catch(console.error);
+    }
+})
